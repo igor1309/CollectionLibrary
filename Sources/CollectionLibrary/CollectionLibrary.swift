19c69cc3e8aeb7d6b58d653d47d7a581f0c5efbb
Igor Malyarov <igor1309@gmail.com>
2020-06-01 13:22:02 +0300

    Initial Commit

---
+++ Sources/CollectionLibrary/CollectionLibrary.swift
@@ -0,0 +1,3 @@
+struct CollectionLibrary {
+    var text = "Hello, World!"
+}
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import CollectionLibraryTests
+
+var tests = [XCTestCaseEntry]()
+tests += CollectionLibraryTests.allTests()
+XCTMain(tests)
